0a3093f29c4d6e143a083e7f3d4f7a52858d70d6
Hardik0131 <hardikbhaliya333@gmail.com>
2026-04-20 18:22:57 +0530

    Create a Recruiter Controller

--- app/Http/Controllers/RecruiterController.php
+++ app/Http/Controllers/RecruiterController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Applications;
+use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RecruiterController extends Controller
 {
-    //
+    public function candidates(){
+        $listings = Listing::where('user_id', Auth::user()->id)->get();
+        $candidates = Applications::where('user_id', Auth::user()->id)->get();
+
+        return view('recruiter.candidates', compact('candidates'));
+    }
 }
